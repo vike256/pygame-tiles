987eadf009f9a2c1feecf5d5db31b212bd7fa21f
vike256 <69812383+vike256@users.noreply.github.com>
2024-12-14 13:37:54 +0200

    Add globals and tile removal

--- src/main.py
+++ src/main.py
@@ -1,42 +1,43 @@
 import pygame
+import globals as g
 from tile import Tile
 
-SCREEN_WIDTH = 480
-SCREEN_HEIGHT = 960
-TILE_WIDTH = SCREEN_WIDTH // 4
-TILE_HEIGHT = TILE_WIDTH // 2
-HIT_POS = SCREEN_HEIGHT - TILE_HEIGHT * 2
-COLUMNS = (0, 0 + TILE_WIDTH, 0 + TILE_WIDTH * 2, 0 + TILE_WIDTH * 3)
+def main():
+    # initialize pygame and related variables
+    pygame.init()
+    screen = pygame.display.set_mode((g.SCREEN_WIDTH, g.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-running = True
-dt = 0
+    hit_surface = pygame.Surface((g.SCREEN_WIDTH, g.TILE_HEIGHT))
+    hit_surface.set_alpha(128)
+    hit_surface.fill('white')
 
-hit_surface = pygame.Surface((SCREEN_WIDTH, TILE_HEIGHT))
-hit_surface.set_alpha(128)
-hit_surface.fill('white')
+    tiles = []
+    tiles.append(Tile())
 
-tiles = []
-tiles.append(Tile())
+    speed = 250
 
-speed = 250
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        screen.fill('#2B2B2B')
 
-    screen.fill('#2B2B2B')
+        for tile in tiles:
+            tile.update(dt, speed)
+            if not tile.alive:
+                tiles.remove(tile)
+            pygame.draw.rect(screen, 'green', (g.COLUMNS[tile.column], tile.y, g.TILE_WIDTH, g.TILE_HEIGHT))
+        print(tiles)
 
-    for tile in tiles:
-        tile.update(dt, speed)
-        pygame.draw.rect(screen, 'green', (COLUMNS[tile.column], tile.y, TILE_WIDTH, TILE_HEIGHT))
-    #pygame.draw.rect(screen, pygame.Color(255, 255, 255, 0), (0, HIT_POS, SCREEN_WIDTH, TILE_HEIGHT))
+        screen.blit(hit_surface, (0, g.HIT_POS))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
-    screen.blit(hit_surface, (0, HIT_POS))
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    pygame.quit()
 
-pygame.quit()
+if __name__ == '__main__':
+    main()
--- src/tile.py
+++ src/tile.py
@@ -1,9 +1,13 @@
+import globals as g
 from random import randint
 
 class Tile:
     def __init__(self):
         self.column = randint(0, 3)
         self.y = 0
+        self.alive = True
 
     def update(self, dt, speed):
         self.y += speed * dt
+        if self.y > g.SCREEN_HEIGHT:
+            self.alive = False
