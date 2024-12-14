c26e77f1f1b0e034659ffa1c609777c46769ce51
vike256 <69812383+vike256@users.noreply.github.com>
2024-12-14 13:07:42 +0200

    Create base view

--- src/main.py
+++ src/main.py
@@ -4,21 +4,28 @@ SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 960
 TILE_WIDTH = SCREEN_WIDTH // 4
 TILE_HEIGHT = TILE_WIDTH // 2
+HIT_POS = SCREEN_HEIGHT - TILE_HEIGHT * 2
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
+hit_surface = pygame.Surface((SCREEN_WIDTH, TILE_HEIGHT))
+hit_surface.set_alpha(128)
+hit_surface.fill('white')
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill('black')
+    screen.fill('#2B2B2B')
 
-    pygame.draw.rect(screen, (0, 255, 0), (0, 0, TILE_WIDTH, TILE_HEIGHT))
+    pygame.draw.rect(screen, 'green', (0, 0, TILE_WIDTH, TILE_HEIGHT))
+    #pygame.draw.rect(screen, pygame.Color(255, 255, 255, 0), (0, HIT_POS, SCREEN_WIDTH, TILE_HEIGHT))
 
+    screen.blit(hit_surface, (0, HIT_POS))
     pygame.display.flip()
     dt = clock.tick(60) / 1000
 
